0fa8e64cd5285eea8e31d5d2db7c25c4d31b40cd
Asakeron <asakeron@gmail.com>
2013-06-27 19:28:27 -0300

    Implemented get-context-attributes and fixed typos

--- src/context.cljs
+++ src/context.cljs
@@ -1,15 +1,16 @@
 (ns gl)
 
 ((fn []
-   (let [default-attributes {:alpha true,
+   (let [default-attributes {:alpha true
                              :depth true,
                              :stencil false,
                              :antialias true,
                              :premultiplied-alpha true,
                              :preserve-drawing-buffer false}
-         attributes->js (fn [attributes]
+         attributes->js (fn 
+                          [attributes]
                           (clj->js {
-                                     :aplha (get attributes :alpha),
+                                     :alpha (get attributes :alpha),
                                      :depth (get attributes :depth),
                                      :stencil (get attributes :stencil),
                                      :antialias (get attributes :antialias),
@@ -19,9 +20,23 @@
      
      (defmulti get-context (fn [& arglist] (count arglist)))
      
-     (defmethod get-context 1 [canvas-element]
+     (defmethod get-context 1
+       [canvas-element]
        (.getContext canvas-element "experimental-webgl"))
 
-     (defmethod get-context 2 [canvas-element context-attributes]
+     (defmethod get-context 2 
+       [canvas-element context-attributes]
        (.getContext canvas-element "experimental-webgl" (attributes->js (merge default-attributes context-attributes))))
      )))
+
+(defn get-context-attributes
+  [gl-context]
+  (let [js-obj (.getContextAttributes gl-context)]
+    {
+     :alpha (.-alpha js-obj),
+     :depth (.-depth js-obj),
+     :stencil (.-stencil js-obj),
+     :antialias (.-antialias js-obj),
+     :premultiplied-alpha (.-premultipliedAlpha js-obj),
+     :preserve-drawing-buffer (.-preserveDrawingBuffer js-obj)
+     }))
