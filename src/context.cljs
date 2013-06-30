e39b89b1842a53e37a068a4db3f8b08f4056f636
asakeron <asakeron@gmail.com>
2013-06-30 17:23:08 -0300

    Removed multimethod for get-context; Renamed namespace.

--- src/context.cljs
+++ src/context.cljs
@@ -1,39 +1,32 @@
-(ns gl)
-
-((fn []
-   (let [default-attributes {:alpha true
-                             :depth true,
-                             :stencil false,
-                             :antialias true,
-                             :premultiplied-alpha true,
-                             :preserve-drawing-buffer false}
-         attributes->js (fn 
-                          [attributes]
-                          (clj->js {
-                                     :alpha (get attributes :alpha),
-                                     :depth (get attributes :depth),
-                                     :stencil (get attributes :stencil),
-                                     :antialias (get attributes :antialias),
-                                     :premultipliedAplha (get attributes :premultiplied-alpha),
-                                     :preserveDrawingBuffer (get attributes :preserve-drawing-buffer)
-                                     }))]
-     
-     (defmulti get-context (fn [& arglist] (count arglist)))
-     
-     (defmethod get-context 1
-       [canvas-element]
-       (.getContext canvas-element "experimental-webgl"))
-
-     (defmethod get-context 2 
-       [canvas-element context-attributes]
-       (.getContext canvas-element "experimental-webgl" (attributes->js (merge default-attributes context-attributes))))
-     )))
+(ns cljswebgl.gl)
+  
+(defn get-context
+  
+  ([canvas-element]
+     (.getContext canvas-element "experimental-webgl"))
+  
+  ([canvas-element context-attributes]
+     (let [default-attributes {:alpha true
+                               :depth true,
+                               :stencil false,
+                               :antialias true,
+                               :premultiplied-alpha true,
+                               :preserve-drawing-buffer false}
+           attributes->js (fn
+                            [{alpha :alpha depth :depth stencil :stencil antialias :antialias
+                              premultiplied-alpha :premultiplied-alpha preserve-drawing-buffer :preserve-drawing-buffer}]
+                            (clj->js {:alpha alpha,
+                                      :depth depth,
+                                      :stencil stencil,
+                                      :antialias antialias,
+                                      :premultipliedAplha premultiplied-alpha,
+                                      :preserveDrawingBuffer preserve-drawing-buffer}))]
+       (.getContext canvas-element "experimental-webgl" (attributes->js (merge default-attributes context-attributes))))))
 
 (defn get-context-attributes
   [gl-context]
   (let [js-obj (.getContextAttributes gl-context)]
-    {
-     :alpha (.-alpha js-obj),
+    {:alpha (.-alpha js-obj),
      :depth (.-depth js-obj),
      :stencil (.-stencil js-obj),
      :antialias (.-antialias js-obj),
