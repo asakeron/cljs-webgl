cb1d1db45b458de721b9ef4c94efa54fc7e06257
Asakeron <asakeron@gmail.com>
2013-07-05 19:44:56 -0300

    Implemented create-buffer

---
+++ src/buffer.cljs
@@ -0,0 +1,9 @@
+(ns cljswebgl.buffer)
+
+(defn create-buffer
+  ;; target may be gl-array-buffer or gl-element-array-buffer
+  [gl-context data target usage]
+  (let [buffer (.createBuffer gl-context)]
+    (.bindBuffer gl-context target buffer)
+    (.bufferData gl-context target data usage)
+    buffer))
