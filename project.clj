4091ae5cd7fadeebc6ff6f02e9bbc04895501da1
André Barnabá <asakeron@gmail.com>
2013-12-09 19:53:56 -0200

    New directory structure.

--- project.clj
+++ project.clj
@@ -1,19 +1,15 @@
-(defproject cljs-webgl "0.1.0-SNAPSHOT"
+(defproject cljs-webgl "0.1.3-SNAPSHOT"
   :description "WebGL binding to ClojureScript"
   :url "https://github.com/Asakeron/cljs-webgl"
-  :license "zlib"
-  :plugins [[lein-cljsbuild "0.3.2"]
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojurescript "0.0-2080"]]
+  :source-paths ["src/cljs"]
+  :plugins [[lein-cljsbuild "1.0.1-SNAPSHOT"]
             [lein-marginalia "0.7.1"]]
-  
-  :cljsbuild {
-              :builds {
-                       
-                       :debug
-                       {:source-paths ["src-cljs"]
-                        :compiler {:output-to "target/cljs-webgl.js"}}
-                       
-                       :release
-                       {:source-paths ["src-cljs"]
-                        :compiler {:output-to "target/cljs-webgl.min.js"
-                                   :optimizations :advanced
-                                   :pretty-print false}}}})
+  :cljsbuild {:builds
+              [{:source-paths ["src/cljs"]
+                :compiler {:output-to "target/cljs-webgl.min.js"
+                           :source-map "target/cljs-webgl.min.js.map"
+                           :optimizations :advanced
+                           :pretty-print false}}]})
