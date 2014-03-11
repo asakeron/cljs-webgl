3c79a72e0f8c03f33b99c8263cdb1bb767eb6054
Richard Hull <rm_hull@yahoo.co.uk>
2014-03-11 22:16:21 +0000

    Update dependent versions

--- project.clj
+++ project.clj
@@ -2,15 +2,15 @@
   :description "WebGL binding to ClojureScript"
   :url "https://github.com/Asakeron/cljs-webgl"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojurescript "0.0-2080"]
-                 [org.clojure/core.match "0.2.0"]]
+	    :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojurescript "0.0-2173"]
+		 [org.clojure/core.match "0.2.1"]]
   :source-paths ["src/cljs"]
-  :plugins [[lein-cljsbuild "1.0.1-SNAPSHOT"]
-            [lein-marginalia "0.7.1"]]
+  :plugins [[lein-cljsbuild "1.0.2"]
+	    [lein-marginalia "0.7.1"]]
   :cljsbuild {:builds
-              [{:source-paths ["src/cljs"]
-                :compiler {:output-to "target/cljs-webgl.min.js"
-                           :source-map "target/cljs-webgl.min.js.map"
-                           :optimizations :advanced
-                           :pretty-print false}}]})
+	      [{:source-paths ["src/cljs"]
+		:compiler {:output-to "target/cljs-webgl.min.js"
+			   :source-map "target/cljs-webgl.min.js.map"
+			   :optimizations :advanced
+			   :pretty-print false}}]})
