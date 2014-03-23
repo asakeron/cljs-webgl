15428659f68fad0aad6ec3bc80de1980630f7579
Richard Hull <rm_hull@yahoo.co.uk>
2014-03-23 23:35:07 +0000

    Added more detail to lesson 1 example

--- examples/src/learningwebgl/lesson_01.cljs
+++ examples/src/learningwebgl/lesson_01.cljs
@@ -1,8 +1,9 @@
 (ns learningwebgl.lesson-01
   (:require
+    [mat4]
     [cljs-webgl.buffers :refer [create-buffer clear-color-buffer draw!]]
     [cljs-webgl.context :refer [get-context]]
-    [cljs-webgl.shaders :refer [get-shader create-program]]
+    [cljs-webgl.shaders :refer [get-shader create-program get-attrib-location]]
     [cljs-webgl.constants :as const]
     [cljs-webgl.typed-arrays :as ta]))
 
@@ -22,53 +23,70 @@
         vertex-shader (get-shader gl "shader-vs")]
     (create-program gl fragment-shader vertex-shader)))
 
+(defn get-perspective-matrix [gl]
+  (mat4/perspective
+    (mat4/create)
+    45
+    (/  (.-viewportWidth gl) (.-viewportHeight gl))
+    0.1
+    100.0))
+
+(defn get-position-matrix [v]
+  (let [m (mat4/create)]
+    (mat4/translate m m v)))
+
 (defn start []
-  (try
-    (let [canvas      (.getElementById js/document "canvas")
-          gl          (init-gl canvas)
-          shader-prog (init-shaders gl)
-          triangle-vertex-buffer
-                      (create-buffer gl
-                        (ta/float32 [ 0.0  1.0  0.0
-                                     -1.0 -1.0  0.0
-                                      1.0 -1.0  0.0 ])
-                        const/array-buffer
-                        const/static-draw)
-          square-vertex-buffer
-                      (create-buffer gl
-                        (ta/float32 [ 1.0,  1.0,  0.0,
-                                     -1.0,  1.0,  0.0,
-                                      1.0, -1.0,  0.0,
-                                     -1.0, -1.0,  0.0])
-                        const/array-buffer
-                        const/static-draw)]
+  (let [canvas      (.getElementById js/document "canvas")
+        gl          (init-gl canvas)
+        shader-prog (init-shaders gl)
+        triangle-vertex-buffer
+                    (create-buffer gl
+                      (ta/float32 [ 0.0  1.0  0.0
+                                   -1.0 -1.0  0.0
+                                    1.0 -1.0  0.0 ])
+                      const/array-buffer
+                      const/static-draw)
+        square-vertex-buffer
+                    (create-buffer gl
+                      (ta/float32 [ 1.0,  1.0,  0.0,
+                                   -1.0,  1.0,  0.0,
+                                    1.0, -1.0,  0.0,
+                                   -1.0, -1.0,  0.0])
+                      const/array-buffer
+                      const/static-draw)
 
-      (clear-color-buffer gl 0.0 0.0 0.0 1.0)
-      (.enable gl const/depth-test)
+        vertex-position-attribute (get-attrib-location gl shader-prog "aVertexPosition")]
 
-      (draw!
-        gl
-        shader-prog
+    (clear-color-buffer gl 0.0 0.0 0.0 1.0)
+    (.enable gl const/depth-test)
 
-        ; draw-mode
-        const/triangles
+    (draw!
+      gl
+      shader-prog
 
-        ; TODO -- below params not yet defined
+      ; draw-mode
+      const/triangles
 
-        0 ; first
-        3 ; count
+      0 ; first
+      (.-numItems triangle-vertex-buffer) ; count
 
-        ; attributes
-        []
+      ; attributes
+      [{:buffer triangle-vertex-buffer
+        :location vertex-position-attribute}]
 
-        ; uniforms
-        []
+      ; uniforms
+      [{:name "uPMatrix" :type :mat4 :values (get-perspective-matrix gl)}
+       {:name "uMVMatrix" :type :mat4 :values (get-position-matrix [-1.5 0.0 -7.0])}]
 
-        ;element-array
-        {}))
+      ;element-array
+      nil
+      ;{:buffer element-buffer
+      ; :count 3
+      ; :type const/unsigned-short
+      ; :offset 0}
+      )))
 
-  (catch js/Error e
-    (js/alert e))))
+
 
 ;        shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
 ;        gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute)
