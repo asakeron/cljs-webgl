bf2861d587fd85598c4344d7e2c52c5429e55b8f
asakeron <asakeron@gmail.com>
2014-03-29 18:29:50 -0300

    Construction of typed arrays for attributes at every call to draw! should not be mandatory.

--- src/cljs/cljs_webgl/buffers.cljs
+++ src/cljs/cljs_webgl/buffers.cljs
@@ -65,29 +65,25 @@
   (.clear gl-context constants/stencil-buffer-bit)
   gl-context)
 
-(defn ^:private bool->float
-  [x]
-  (if x 1.0 0.0))
-
 (defn ^:private set-uniform
   [gl-context shader {:keys [name type values transpose]}]
   (let [uniform-location (shaders/get-uniform-location gl-context shader name)]
     (case type
-      :bool   (.uniform1fv gl-context uniform-location (ta/float32 (map bool->float values)))
-      :bvec2  (.uniform2fv gl-context uniform-location (ta/float32 (map bool->float values)))
-      :bvec3  (.uniform3fv gl-context uniform-location (ta/float32 (map bool->float values)))
-      :bvec4  (.uniform4fv gl-context uniform-location (ta/float (map bool->float values)))
-      :float  (.uniform1fv gl-context uniform-location (ta/float32 values))
-      :vec2   (.uniform2fv gl-context uniform-location (ta/float32 values))
-      :vec3   (.uniform3fv gl-context uniform-location (ta/float32 values))
-      :vec4   (.uniform4fv gl-context uniform-location (ta/float32 values))
-      :int    (.uniform1iv gl-context uniform-location (ta/int32 values))
-      :ivec2  (.uniform2iv gl-context uniform-location (ta/int32 values))
-      :ivec3  (.uniform3iv gl-context uniform-location (ta/int32 values))
-      :ivec4  (.uniform4iv gl-context uniform-location (ta/int32 values))
-      :mat2   (.uniformMatrix2fv gl-context uniform-location transpose (ta/float32 values))
-      :mat3   (.uniformMatrix3fv gl-context uniform-location transpose (ta/float32 values))
-      :mat4   (.uniformMatrix4fv gl-context uniform-location transpose (ta/float32 values))
+      :bool   (.uniform1fv gl-context uniform-location values)
+      :bvec2  (.uniform2fv gl-context uniform-location values)
+      :bvec3  (.uniform3fv gl-context uniform-location values)
+      :bvec4  (.uniform4fv gl-context uniform-location values)
+      :float  (.uniform1fv gl-context uniform-location values)
+      :vec2   (.uniform2fv gl-context uniform-location values)
+      :vec3   (.uniform3fv gl-context uniform-location values)
+      :vec4   (.uniform4fv gl-context uniform-location values)
+      :int    (.uniform1iv gl-context uniform-location values)
+      :ivec2  (.uniform2iv gl-context uniform-location values)
+      :ivec3  (.uniform3iv gl-context uniform-location values)
+      :ivec4  (.uniform4iv gl-context uniform-location values)
+      :mat2   (.uniformMatrix2fv gl-context uniform-location transpose values)
+      :mat3   (.uniformMatrix3fv gl-context uniform-location transpose values)
+      :mat4   (.uniformMatrix4fv gl-context uniform-location transpose values)
       nil)))
 
 (defn ^:private set-attribute
